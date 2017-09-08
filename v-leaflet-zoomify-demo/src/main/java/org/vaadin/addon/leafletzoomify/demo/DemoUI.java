5932a6d17e937443d8b1d6e62bf77b07efef4c9e
dve <dve@vergien.net>
2017-09-08 15:57:56 +0200

    Fix projection

--- v-leaflet-zoomify-demo/src/main/java/org/vaadin/addon/leafletzoomify/demo/DemoUI.java
+++ v-leaflet-zoomify-demo/src/main/java/org/vaadin/addon/leafletzoomify/demo/DemoUI.java
@@ -3,16 +3,12 @@ package org.vaadin.addon.leafletzoomify.demo;
 import javax.servlet.annotation.WebServlet;
 
 import org.vaadin.addon.leaflet.LMap;
+import org.vaadin.addon.leaflet.shared.Crs;
 import org.vaadin.addon.leafletzoomify.LZoomifiyLayer;
 
-import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.Title;
-import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.annotations.*;
+import com.vaadin.server.*;
+import com.vaadin.ui.*;
 
 @Theme("demo")
 @Title("LZoomifyLayer Add-on Demo")
@@ -29,11 +25,11 @@ public class DemoUI extends UI {
 
 		// Initialize our new UI component
 		final LMap leafletMap = new LMap();
-
+		leafletMap.setCrs(Crs.Simple);
 		leafletMap.setHeight("100%");
-		leafletMap.setCenter(0, 0);
-		leafletMap.setMinZoom(0);
-		leafletMap.setMaxZoom(5);
+
+		leafletMap.setView(0d, 0d, 0d);
+
 		LZoomifiyLayer layer = new LZoomifiyLayer(
 				"http://cmulders.github.io/Leaflet.Zoomify/examples/hubble-orion-nebula/{g}/{z}-{x}-{y}.jpg", 6000,
 				6000, "&copy; NASA, ESA, M. Robberto and the Hubble Space Telescope Orion Treasury Project Team'");
