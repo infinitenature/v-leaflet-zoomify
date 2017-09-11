10f3aebe3be2b40709d2373c5dfe193ab30016dd
dve <dve@vergien.net>
2017-09-11 11:58:35 +0200

    Add automated test

---
+++ src/test/java/org/infinitenature/leafletzoomify/SimpleTest.java
@@ -0,0 +1,41 @@
+package org.infinitenature.leafletzoomify;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.vaadin.addonhelpers.TListUi;
+import org.vaadin.addonhelpers.automated.AbstractWebDriverCase;
+
+import java.util.List;
+
+public class SimpleTest extends AbstractWebDriverCase {
+
+    @Test
+    public void checkAllTestsOpenWithoutErrors() throws IOException, AssertionError {
+        startBrowser();
+
+        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+        
+        List<TListUi.TestDetails> listTestClasses = TListUi.listTestClasses();
+        for (TListUi.TestDetails td : listTestClasses) {
+			Class clazz = td.getClazz();
+			
+			driver.get(BASEURL + clazz.getName() + "?debug");
+			waitForLoading();
+			try {
+				WebElement error = driver.findElement(By.className("v-Notification-error"));
+				Assert.fail("Test " + clazz.getName() + " has client side exception");
+			} catch (NoSuchElementException e) {
+				continue;
+			}
+			
+		}
+        
+    }
+
+}
