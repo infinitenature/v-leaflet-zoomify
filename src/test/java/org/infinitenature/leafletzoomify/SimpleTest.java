92383c957780144fb2ea2b09560555e11dc5b981
dve <dve@vergien.net>
2017-09-11 13:16:17 +0200

    Use correct wait

--- src/test/java/org/infinitenature/leafletzoomify/SimpleTest.java
+++ src/test/java/org/infinitenature/leafletzoomify/SimpleTest.java
@@ -9,8 +9,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.vaadin.addonhelpers.TListUi;
-import org.vaadin.addonhelpers.automated.AbstractWebDriverCase;
+import org.vaadin.addonhelpers.automated.*;
 
 import java.util.List;
 
@@ -27,7 +28,9 @@ public class SimpleTest extends AbstractWebDriverCase {
 			Class clazz = td.getClazz();
 			
 			driver.get(BASEURL + clazz.getName() + "?debug");
-			waitForLoading();
+
+			new WebDriverWait(driver, 30).until(VaadinConditions.ajaxCallsCompleted());
+
 			try {
 				WebElement error = driver.findElement(By.className("v-Notification-error"));
 				Assert.fail("Test " + clazz.getName() + " has client side exception");
